f40305ba9c1df84c40dc9a4112ef314592a315aa
Max <sokol.max777@gmail.com>
2022-08-18 09:21:26 +0300

    res to file

--- main.py
+++ main.py
@@ -5,66 +5,73 @@ from lxml import etree as et
 import json
 import requests
 import time
+from datetime import datetime, date, timezone
+import pytz
+import maya
 
 
 vinted = Vinted()
 # search(url, number_of_items, page_number)
-
 # newest_resp = vinted.items.search("https://www.vinted.pl/vetements?search_text=Smartwatch&order=newest_first", 20, i)
 
-newest = []
-low_rate_members = []
+days = 86400 * 2
 
 
 def get_newest():
 
     i = 1
-    while i < 80:
+    while i < 440:
         try:
             newest_resp = vinted.items.search("https://www.vinted.pl/vetements?search_text=&order=newest_first", 10, i)
             for item in newest_resp:
-                newest.append(item.url)
-            time.sleep(3)
+                check_items(item.url)
+            # time.sleep(2)
             i += 1
-            print("get newest", len(newest))
         except:
             pass
 
 
-def check_items():
-    for item in newest:
-        try:
-            get_item = requester.get(str(item))
-            item_soup = BS(get_item.text, "html.parser")
-            item_json = item_soup.select_one('script[data-component-name="ItemViewItems"]').text
+def check_items(item):
+    try:
+        get_item = requester.get(str(item))
+        item_soup = BS(get_item.text, "html.parser")
+        item_json = item_soup.select_one('script[data-component-name="ItemViewItems"]').text
 
-            res_jsn = json.loads(str(item_json))
+        res_jsn = json.loads(str(item_json))
 
-            member_url = res_jsn['items'][1]['user']['profile_url']
-            member_id = str(res_jsn['items'][1]['user']['id'])
+        member_id = str(res_jsn['items'][1]['user']['id'])
+        member_resp = requester.get(f'https://www.vinted.pl/api/v2/users/{str(member_id)}').text
+        member_json = json.loads(str(member_resp))
+        member_url = member_json['user']['profile_url']
+        amount_member_items = member_json['user']['item_count']
+        total_items_count = member_json['user']['total_items_count']
+        positive_feedback = member_json['user']['positive_feedback_count']
+        neutral_feedback = member_json['user']['neutral_feedback_count']
+        negative_feedback = member_json['user']['negative_feedback_count']
+        country = member_json['user']['country_code']
+        created_at = maya.parse(member_json['user']['created_at']).datetime()
+        last_loged = member_json['user']['last_loged_on_ts']
 
-            get_member = requester.get(member_url)
-            member_items_resp = requester.get(f'https://www.vinted.pl/api/v2/users/{str(member_id)}/items?page=1&per_page=20&order=relevance&currency=PLN').text
-            member_items_jsn = json.loads(str(member_items_resp))
-            amount_member_items = member_items_jsn['pagination']['total_entries']
+        print(member_id)
 
-            if amount_member_items <= 3:
-                # low_rate_members.append(member_url)
-                with open("result.txt", "w") as file:
-                    file.write(member_url + '\n')
-            time.sleep(2)
-            print("check rate", newest.index(item))
+        if country == 'PL':
+            if positive_feedback == 0 and  neutral_feedback == 0 and negative_feedback == 0:
+                if ((datetime.now(pytz.utc) - created_at).total_seconds()) < days:
+                    if amount_member_items < 7:
+                        res = (f"""URL: {member_url}
+                            Items: {total_items_count}
+                            Was register:{created_at}
+                            Country:{country}""")
 
-        except:
-            pass
+                        print(res)
+                        with open("result.txt", "a") as file:
+                            file.write(res + '\n')
+
+
+    except:
+        pass
 
 
 get_newest()
-check_items()
-
-print(low_rate_members)
-
-# print(len(newest))
-# print(amount_member_items)
 
 
